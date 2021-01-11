659015d91f17a3fdee8c37f41100d3c062c5d6bf
J. Steve Louis <j.s.louis@outlook.com>
2021-01-11 02:24:48 -0500

    Add env vars for FE client API endpoints

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,8 @@ import https from 'https';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    hostname: 'http://localhost:3000/',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+    hostname: process.env.REEACT_APP_API_HOST || 'http://localhost:3000/',
     httpsAgent: https.Agent({
         rejectUnauthorized: false,
     }),
