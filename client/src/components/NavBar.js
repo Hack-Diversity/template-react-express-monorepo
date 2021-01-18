8aae693fc70172c6af6dc6a27fab4f785e8c7bb8
Andy Maskiell <maskiella@gmail.com>
2021-01-18 12:50:30 -0500

    Make more styling fixes for mobile

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import Logo from './Logo';
 import Links from './Links';
 
 const Container = styled.div.attrs({
@@ -16,24 +15,17 @@ const Nav = styled.nav.attrs({
     className: 'navbar navbar-expand-lg navbar-dark bg-dark',
 })`
     margin-bottom: 20px;
-    max-height: 5em;
 
     @media screen and (min-width: 992px) {
         padding: 0.5em 25%;
     }
 `;
 
-const logoStyles = {
-    height: '40px',
-    width: '40px',
-};
-
 class NavBar extends Component {
     render() {
         return (
             <Container>
                 <Nav>
-                    <Logo logoStyles={logoStyles} />
                     <Links />
                 </Nav>
             </Container>
