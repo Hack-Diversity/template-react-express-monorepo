a01b8fe10ea693017bce6d8c2b9f372396dda631
m33dusa <40845110+m33dusa@users.noreply.github.com>
2021-01-29 19:14:10 -0600

    Testing merge localization
    Added new <p> character. Testing commit location

--- client/src/components/PageLayout.js
+++ client/src/components/PageLayout.js
@@ -6,6 +6,7 @@ const PageLayout = () =>
     <div className="page-layout--header">
         <div className="page-layout--details">
             <h1 className="page-layout--name">Hack.Diversity React/Redux Template</h1>
+            <h1 className="page-layout--name">Hack.Monday React/Redux Template</h1>
             <p className="page-layout--description">
                 A simple CRUD app built using the&nbsp;
                 <a href="https://www.educative.io/edpresso/what-is-mern-stack" target="_blank" rel="noopener noreferrer">MERN&nbsp;tech-stack</a>.
