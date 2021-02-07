2f3dc9c52f4fca1e9025ce6dd255ea2d39e3089e
J. Steve Louis <jslouis@takeoffreadyconsulting.com>
2021-02-07 12:43:26 -0500

    Add MONGO_CONNECTION_STRING environment variable

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const connection_string = process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/items'
+
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/items', { useNewUrlParser: true })
+    .connect(connection_string, { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message);
     });
